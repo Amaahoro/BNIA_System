823b4dda1570f7ed2de3d7b68b5ca1d3c7badfaf
nkurudavid <nkurudavid44@gmail.com>
2023-08-16 04:20:02 +0200

    created simple views

--- management/views.py
+++ management/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.conf import settings
+
+from .models import *
+
 
 # Create your views here.
+def handle_not_found(request, exception):
+    return render(request, 'management/404.html')
+
+
+
+def home(request):
+    return render(request, 'management/index.html')
+
